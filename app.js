c44269a8d91d262220ad9ef66b28b353bfa31941
Martin Juhl <m@rtinjuhl.dk>
2021-12-26 00:58:14 +0100

    Added connection check on app start.. Package bump

--- app.js
+++ app.js
@@ -16,14 +16,30 @@ if (fs.existsSync(__dirname+'/config.js')) {
    process.exit(1)
 }
 
-// Create an OnvifDevice object
-let device = new onvif.OnvifDevice({
-  address: config.address,
-  user : config.user,
-  pass : config.pass,
-});
+function requestRetry() {
+  let devicetemp = new onvif.OnvifDevice({address: config.address, user : config.user, pass : config.pass});
+  console.log("Start");
+  function run() {
+	  console.log("Running");
+          devicetemp.init().then((info) => {
+          // Show the detailed information of the device.
+             console.log(JSON.stringify(info, null, '  '));
+             console.log("Connected");
+             return devicetemp;
 
-device.init();
+          }).catch((error) => {
+             console.error(error);
+             console.log("Retrying");
+             setTimeout(run, 2000);
+          });
+
+  }
+  run();
+
+}
+
+
+device = requestRetry(); 
 
 app.use(express.json());
 
@@ -37,7 +53,7 @@ app.post('/api/', function(req, res){
    res.send(req.body);    // echo the result back
 });
 
-glob.sync( './routes/*.js' ).forEach( function( file ) {
+glob.sync(__dirname+'/routes/*.js' ).forEach( function( file ) {
    console.log("Loading operation: "+file);
    eval(fs.readFileSync(file)+'');
 });
